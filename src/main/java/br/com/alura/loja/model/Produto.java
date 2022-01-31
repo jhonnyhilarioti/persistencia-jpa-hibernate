9d0889b8d7eb4e0798eaafae5c44d0d6f2104838
Jhonny Hilário Goulart <jhonnyhilarioti@gmail.com>
2022-01-31 15:02:27 -0300

    Configurando o arquivo persistence.xml e mapeando a entidade produto

---
+++ src/main/java/br/com/alura/loja/model/Produto.java
@@ -0,0 +1,48 @@
+package br.com.alura.loja.model;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private BigDecimal preco;
+    private String nome;
+    private String descricao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
